e3cfadfed535d48eb3b8ec8c7776ef2eab656b92
SerhatYaren09 <64776783+SerhatYaren09@users.noreply.github.com>
2021-04-06 12:09:59 +0300

    Oyun Projesi

---
+++ Assets/SayiTahmini.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SayiTahmini : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int max = 1500;
+        int min = 1;
+        Debug.Log("Sayi Tahmini Oyunu Baþlýyor...");
+        Debug.Log("Aklýndan bir sayý tut ve bunu bana sakýn söyleme.");
+        Debug.Log("Tuttuðun sayý "+ max + " geçmesin.");
+        Debug.Log("Tuttuðun sayý "+ min + " den aþaðý olmasýn.");
+        Debug.Log("Tuttuðun sayý 750 mi?");
+        Debug.Log("Yukarý bas = Yükselt , Aþaðý bas = Azalt , Enter'a bas = Doðru");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
